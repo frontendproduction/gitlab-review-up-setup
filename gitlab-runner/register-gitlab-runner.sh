d685ddd55a1deb29ad4542c35b83aa5232f6176e
laland <laland@ya.ru>
2022-03-20 16:38:49 +0300

    Fix runners config and add config.toml

--- gitlab-runner/register-gitlab-runner.sh
+++ gitlab-runner/register-gitlab-runner.sh
@@ -4,10 +4,20 @@ export GITLAB_TOKEN=5Nz-y8eVHUkvyT6KrkYU
 
 gitlab-runner \
     register -n \
-    --name "FP Review App Runner" \
+    --name "FP Swarm Shell" \
     --executor shell \
     --docker-image docker:latest \
     --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
     --url $GITLAB_URL \
     --registration-token $GITLAB_TOKEN \
     --tag-list review-app-swarm
+
+gitlab-runner \
+    register -n \
+    --name "FP Swarm Docker" \
+    --executor docker \
+    --docker-image docker:latest \
+    --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+    --url $GITLAB_URL \
+    --registration-token $GITLAB_TOKEN \
+    --tag-list docker
