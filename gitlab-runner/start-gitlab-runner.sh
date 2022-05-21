9aa88ae1b0cb645fd9125cce596afc984f4c23c0
laland <laland@ya.ru>
2022-05-21 15:03:44 +0300

    Set gitlab-runner concurrency to 5

--- gitlab-runner/start-gitlab-runner.sh
+++ gitlab-runner/start-gitlab-runner.sh
@@ -2,7 +2,7 @@
 docker run -d \
     --name gitlab-runner \
     --restart always \
-    --concurrent 10 \
+    --concurrent 5 \
     -v gitlab-runner:/etc/gitlab-runner \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /usr/bin/docker:/usr/bin/docker \
