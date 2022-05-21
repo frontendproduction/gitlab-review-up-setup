5ac2befe7580e323610885145dbe4ed3d0a25725
laland <laland@ya.ru>
2022-05-21 18:48:06 +0300

    [gitlab-runner] Add config file to starter script, set concurrency level in config, not in flags

--- gitlab-runner/start-gitlab-runner.sh
+++ gitlab-runner/start-gitlab-runner.sh
@@ -2,8 +2,8 @@
 docker run -d \
     --name gitlab-runner \
     --restart always \
-    --concurrent 5 \
     -v gitlab-runner:/etc/gitlab-runner \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /usr/bin/docker:/usr/bin/docker \
+    -v ${PWD}/config.toml:/etc/gitlab-runner/config.toml \
     gitlab/gitlab-runner:latest
